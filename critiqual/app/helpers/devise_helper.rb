cd5aa15acf67fb2b6cb755667e688c92d81a2996
richard-to <gohn76@gmail.com>
2015-03-28 19:56:48 -0700

    Add less-bootstrap and devisegems

---
+++ critiqual/app/helpers/devise_helper.rb
@@ -0,0 +1,22 @@
+
+# Source: http://stackoverflow.com/questions/10241517/how-to-customize-devise-error-messages-with-classes
+module DeviseHelper
+ def devise_error_messages!
+  return '' if resource.errors.empty?
+
+   messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+   sentence = I18n.t('errors.messages.not_saved',
+   count: resource.errors.count,
+   resource: resource.class.model_name.human.downcase)
+
+   html = <<-HTML
+   <div class="alert alert-danger alert-block"> <button type="button"
+    class="close" data-dismiss="alert">x</button>
+    <h4>#{sentence}</h4>
+    #{messages}
+   </div>
+   HTML
+
+   html.html_safe
+ end
+end
